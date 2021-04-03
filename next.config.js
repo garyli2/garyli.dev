b2395cde7113dcdb8199f0b8f7314577ab3c0553
Gary Li <gary.li1@uwaterloo.ca>
2021-04-03 14:38:38 -0400

    feat(deps): add next bundle analyzer support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
     images: {
-      domains: ['cdn.glitch.com', 'steps4news.web.app', 'semestershare.com'],
+        domains: ["cdn.glitch.com", "steps4news.web.app", "semestershare.com"],
     },
     future: {
-      webpack5: true,
+        webpack5: true,
     },
-  }
+});
