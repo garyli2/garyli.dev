ff2549d232a60d7b7c090d95877043f51d3a7cf5
Gary Li <gary.li1@uwaterloo.ca>
2021-04-04 14:21:33 -0400

    refactor: rewrite TabPanel as functional component

--- components/TabPanel.jsx
+++ components/TabPanel.jsx
@@ -1,53 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 
-class TabPanel extends React.PureComponent {
-    state = { activeIndex: 0 };
+export function TabPanel() {
+    const [activeIndex, setActiveIndex] = useState(0);
 
-    handleChange = (_, activeIndex) => this.setState({ activeIndex });
-    render() {
-        const { activeIndex } = this.state;
-        return (
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center"
-                }}
-            >
-                <div style={{ display: "flex", width: "80%" }}>
-                    <Tabs value={activeIndex} onChange={this.handleChange}>
-                        <Tab label="frontend" />
-                        <Tab label="backend" />
-                    </Tabs>
+    const handleChange = (_, activeIndex) => setActiveIndex(activeIndex);
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center"
+            }}
+        >
+            <div style={{ display: "flex", width: "80%" }}>
+                <Tabs value={activeIndex} onChange={handleChange}>
+                    <Tab label="frontend" />
+                    <Tab label="backend" />
+                </Tabs>
 
-                    {activeIndex === 0 && (
-                        <TabContainer>
-                            <ul>
-                                <li>ReactJS</li>
-                                <li>Material-UI</li>
-                                <li>Bootstrap</li>
-                                <li>JQuery</li>
-                                <li>HTML5</li>
-                            </ul>
-                        </TabContainer>
-                    )}
-                    {activeIndex === 1 && (
-                        <TabContainer>
-                            <ul>
-                                <li>Java</li>
-                                <li>Spring Boot</li>
-                                <li>Hibernate</li>
-                                <li>MySQL</li>
-                                <li>Node.js</li>
-                            </ul>
-                        </TabContainer>
-                    )}
-                </div>
+                {activeIndex === 0 && (
+                    <TabContainer>
+                        <ul>
+                            <li>ReactJS</li>
+                            <li>Material-UI</li>
+                            <li>Bootstrap</li>
+                            <li>JQuery</li>
+                            <li>HTML5</li>
+                        </ul>
+                    </TabContainer>
+                )}
+                {activeIndex === 1 && (
+                    <TabContainer>
+                        <ul>
+                            <li>Java</li>
+                            <li>Spring Boot</li>
+                            <li>Hibernate</li>
+                            <li>MySQL</li>
+                            <li>Node.js</li>
+                        </ul>
+                    </TabContainer>
+                )}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 function TabContainer(props) {
