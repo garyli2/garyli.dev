6a195af18cc564a0749872c8b4f34ece13563769
Gary Li <gary.li1@uwaterloo.ca>
2021-04-06 12:43:06 -0400

    refactor: remove css baseline

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 
 import "../styles.css";
@@ -32,8 +31,6 @@ export default function App(props) {
                 <meta name="viewport" content="width=1024" />
             </Head>
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
                 <Component {...pageProps} />
             </ThemeProvider>
         </React.Fragment>
