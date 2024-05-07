410bbdf75bd634212a7e2b3d0513945e1edfc918
Gary Li <gary.li1@uwaterloo.ca>
2024-05-07 10:46:07 -0400

    Make heading more responsive for smaller screens

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -16,11 +16,11 @@ const Navigation = () => {
 
 const Header = () => {
   return (
-    <div className="mx-auto bg-white px-4 py-10">
+    <div className="mx-auto bg-white py-10">
       <div className="mx-auto flex max-w-screen-md flex-col items-center justify-center">
-        <div className="flex flex-row items-baseline gap-3 md:gap-5">
-          <h1 className="text-4xl font-extrabold text-black md:text-7xl">Gary Li</h1>
-          <h2 className="overflow-hidden whitespace-nowrap text-lg text-gray-600 md:text-4xl">
+        <div className="flex flex-col items-center gap-3 md:flex-row md:items-baseline md:gap-5">
+          <h1 className="text-nowrap text-6xl font-extrabold text-black md:text-7xl">Gary Li</h1>
+          <h2 className="overflow-hidden whitespace-nowrap text-3xl text-gray-600 md:text-4xl">
             Full Stack Developer
           </h2>
         </div>
